58e3b0b7cfb277533bc0a993e30ba23d94610a04
Juan Francisco <jfoa92@hotmail.com>
2020-08-21 16:05:39 -0500

    Transformacion personalizada

--- my_custom_sklearn_transforms/sklearn_transformers.py
+++ my_custom_sklearn_transforms/sklearn_transformers.py
@@ -14,3 +14,18 @@ class DropColumns(BaseEstimator, TransformerMixin):
         data = X.copy()
         # Retornamos um novo dataframe sem as colunas indesejadas
         return data.drop(labels=self.columns, axis='columns')
+
+class ReplaceNaNAndZeros(BaseEstimator, TransformerMixin):
+    def __init__(self, value):
+        self.value = value
+
+    def fit(self, X, y=None):
+        self.fill = pd.Series([X[c].value_counts().index[0]
+                               if X[c].dtype == np.dtype('O') else X[c].mean() for c in X],
+                              index=X.columns)
+        return self
+
+    def transform(self, X):
+        data = X.copy()
+        data = data.replace(0, np.nan, inplace=True)
+        return X.fillna(self.fill)
